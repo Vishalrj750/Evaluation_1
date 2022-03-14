82fb3eb185f00d2839d125a6af5871c700326abc
Vishal Kumar Singh <vishalsingh8574031285@gmail.com>
2022-03-14 21:11:16 +0530

    Mission Complete

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -5,11 +5,13 @@ function Wishlist() {
 
   const [query, setQuery] = React.useState("")
   const [items, setItems] = React.useState([])
+  const [count, setCount] = React.useState(0)
 
   const handleChange = ( event ) => {
     const { value } = event.target;
     setQuery( value );
   };
+
 
   const handleAdd = () => {
     const payLoad = {
@@ -18,18 +20,20 @@ function Wishlist() {
     };
     let newItems = [...items, payLoad];
     setItems( newItems );
+    setCount(count + 1)
   };
 
   return (
     <div className="Wishlist">
       <h1>Wishlist</h1>
-      <input type={ "text" } placeholder="Enter Something" onChange={ handleChange } />
+      { count >= 4 ? <h1>Sorry! You can't add more than 3 items in Wishlist</h1> :  <div><input type={ "text" } placeholder="Enter Something" onChange={ handleChange } />
       <button onClick={ handleAdd }>ADD</button>
       <div>
         { items.map((prod) => {
           return <div>{ prod.title }</div>
         }) }
-      </div>
+      </div></div>}
+      
     </div>
   );
 };
